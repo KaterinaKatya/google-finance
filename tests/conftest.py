5bf55d1d27f24bc9ec61e9e4c7a49eac2a15d764
katya <katya.shalimova@e2log.com>
2024-09-17 15:13:58 -0400

    headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,7 @@ def driver(request):
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--remote-debugging-port=9222')  # Important!
+    chrome_options.add_argument('--remote-debugging-port=9222')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--window-size=1920x1080')
 
