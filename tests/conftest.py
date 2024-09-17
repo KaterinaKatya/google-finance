1d0d3b2a9dbb1204e2c2fe2e22b616078caeab14
katya <katya.shalimova@e2log.com>
2024-09-17 13:30:39 -0400

    driver modified

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,7 @@ url = "https://www.google.com/finance/"
 @pytest.fixture(scope='function')
 def driver(request):
     chrome_options = Options()
+    chrome_options.add_argument('--remote-debugging-pipe')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
